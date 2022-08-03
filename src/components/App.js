6785f23ac62ba4cd7070bfb5b3ead63e2c211d0e
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-08-03 16:32:11 +0530

    jsdfy

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./../styles/App.css";
 
-function App() 
-{
+function App() {
+
+	const [text,settext]=useState("");
+	const [list,setlist]=useState([]);
+
+	useEffect(()=>{
+		console.log(list)
+		
+
+	},[list])
+
+	var num=0;
+	const addbtn=(e)=>{
+		if(text.length!=0){
+			setlist([...list,{id:list.length,text:text}])
+
+		}
+	}
+	const remove=(e)=>{
+		
+		const updatearr=list.filter((element)=>{
+			return	element.id!=e.target.id
+		})
+		setlist(updatearr)
+	
+		
+		
+		
+		
+	}
+	const edit=(e)=>{
+		
+		
+		
+		
+	}
+
+
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+	
+		<input id="task" onChange={(e)=>{settext(e.target.value)}} type="text"></input>
+		<button id="button" onClick={(e)=>{addbtn(e)}}>Add</button>
+
+		<ul>{
+				list.map((ele)=>{
+					return (<>
+					<li key={ele.id} className="list" >{ele.text}</li>
+					<button id={ele.id} onClick={(e)=>{edit(e)}}	type="button">Edit</button>
+					<button id={ele.id} onClick={(e)=>{remove(e)}}	type="button">Delete</button>
+					</>
+					)
+				})
+
+			}
+
+		</ul>
+	
+	
 	</div>
 	);
 }
